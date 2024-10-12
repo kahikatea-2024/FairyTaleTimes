23df7c5a095259ede60e68fc45b03ab495a0c60c
roberthall <rhall477@gmail.com>
2024-10-12 13:08:32 +1300

    nav-bar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'md-lg': { min: '1020px', max: '1250px' },
+      },
+    },
   },
   plugins: [],
 }
